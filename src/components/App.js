82a660dd3e603a98b2c85e5c64e223e1fda11595
felixNyalenda <felixasejunior@gmail.com>
2023-04-17 16:20:54 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { About, Home, NavBar } from ".";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+   return (
+      <div>
+         <NavBar />
+         <Home />
+         <About />
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { default as Home } from "./Home";
+import { default as About } from "./About";
+import { default as NavBar } from "./NavBar";
+
+export { Home, NavBar, About };
